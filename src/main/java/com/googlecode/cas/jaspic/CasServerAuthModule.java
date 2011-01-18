1f9b227e40c5da83d3ba5695507fb90b7972f3b8
hisaaki.sato@gmail.com <hisaaki.sato@gmail.com@1f82b737-0d00-80d6-40f8-2b71f490f041>
2011-01-18 06:46:24 +0000

    Refactoring the module. Fixed to add caller & group principals.

--- src/main/java/com/googlecode/cas/jaspic/CasServerAuthModule.java
+++ src/main/java/com/googlecode/cas/jaspic/CasServerAuthModule.java
@@ -1,18 +1,30 @@
 package com.googlecode.cas.jaspic;
 
+import static javax.security.auth.message.AuthStatus.SEND_CONTINUE;
+import static javax.security.auth.message.AuthStatus.SEND_FAILURE;
+import static javax.security.auth.message.AuthStatus.SEND_SUCCESS;
+import static javax.security.auth.message.AuthStatus.SUCCESS;
 import static org.jasig.cas.client.util.AbstractCasFilter.CONST_CAS_ASSERTION;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.message.AuthException;
 import javax.security.auth.message.AuthStatus;
 import javax.security.auth.message.MessageInfo;
 import javax.security.auth.message.MessagePolicy;
+import javax.security.auth.message.callback.CallerPrincipalCallback;
+import javax.security.auth.message.callback.GroupPrincipalCallback;
 import javax.security.auth.message.module.ServerAuthModule;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,119 +36,81 @@ import org.jasig.cas.client.util.CommonUtils;
 import org.jasig.cas.client.validation.Assertion;
 import org.jasig.cas.client.validation.TicketValidationException;
 
+@SuppressWarnings({ "rawtypes", "unused" })
 public class CasServerAuthModule implements ServerAuthModule {
 
-	protected static final Class[] supportedMessageTypes = new Class[] {
+	private static final String AUTH_TYPE_INFO_KEY = "javax.servlet.http.authType";
+
+	private static final String PROPERTY_SERVER_NAME = "serverName";
+	private static final String PROPERTY_SERVICE = "service";
+	private static final String PROPERTY_ARTIFACT_PARAMATER_NAME = "artifactParameterName";
+	private static final String PROPERTY_SERVICE_PARAMATER_NAME = "serviceParameterName";
+	private static final String PROPERTY_CAS_SERVER_URL_PREFIX = "casServerUrlPrefix";
+	private static final String PROPERTY_CAS_SERVER_LOGIN_URL = "casServerLoginUrl";
+	private static final String PROPERTY_JAAS_CONTEXT = "jaas-context";
+	private static final String PROPERTY_DEFAULT_GROUPS = "defaultGroups";
+	private static final String PROPERTY_GROUP_ATTRIBUTE_NAMES = "groupAttributeNames";
+
+	private static final Class[] supportedMessageTypes = new Class[]{
 			HttpServletRequest.class, HttpServletResponse.class };
+
+	private static Logger logger = Logger.getLogger(CasServerAuthModule.class
+			.getName());
 
 	private MessagePolicy requestPolicy;
 	private MessagePolicy responsePolicy;
 	private CallbackHandler handler;
 	private Map options;
-	private String serverName = null;
-	private String casUrl = null;
-	private String casLoginUrl = null;
-	private String realmName = null;
-	private String defaultGroup[] = null;
-	private String service = null;
-	private boolean sfc = false;
-	private String serviceParameterName = "service";
-	private static final String SERVER_NAME = "serverName";
-	private static final String CAS_URL = "cas-url";
-	private static final String CAS_LOGIN_URL = "cas-login-url";
-	private static final String SUCCESS_FOR_CONTINUE = "success-for-continue";
-	private static final String REQUEST_TICKET = "ticket";
-	private static final String REALM_PROPERTY_NAME = "realm-name";
-	private static final String GROUP_PROPERTY_NAME = "group-name";
-	private static final String BASIC = "Basic";
-	static final String LOCATION_HEADER = "Location";
-	static final String SESSION_ASSERTION = "session-assertion";
-	
-	public CasServerAuthModule(){}
-	
-	public CasServerAuthModule(String str){}
 
-	public void initialize(MessagePolicy reqPolicy, MessagePolicy resPolicy,
-			CallbackHandler cBH, Map opts) throws AuthException {
-		requestPolicy = reqPolicy;
-		responsePolicy = resPolicy;
-		handler = cBH;
-		options = opts;
-		if (options != null) {
-			serverName = (String) options.get(SERVER_NAME);
-			casUrl = (String) options.get(CAS_URL);
-			casLoginUrl = (String) options.get(CAS_LOGIN_URL);
-			sfc = Boolean.parseBoolean((String)options.get(SUCCESS_FOR_CONTINUE));
-			realmName = (String) options.get(REALM_PROPERTY_NAME);
-			if (options.containsKey(GROUP_PROPERTY_NAME)) {
-				defaultGroup = new String[] { (String) options
-						.get(GROUP_PROPERTY_NAME) };
-			}
-		}
+	private String service = null;
+	private String serverName = null;
+	private String artifactParameterName = "ticket";
+	private String serviceParameterName = "service";
+	private boolean encodeServiceUrl = false;
+
+	private boolean redirectAfterValidation = true;
+	private String casServerUrlPrefix = null;
+	private String casServerLoginUrl = null;
+	private boolean renew = false;
+	private boolean gateway = false;
+
+	private String jaasContext = "cas";
+	private String defaultGroups[] = null;
+	private String groupAttributeNames[] = null;
+
+	public CasServerAuthModule() {
 	}
 
+	public CasServerAuthModule(String str) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * javax.security.auth.message.module.ServerAuthModule#getSupportedMessageTypes
+	 * ()
+	 */
 	public Class[] getSupportedMessageTypes() {
 		return supportedMessageTypes;
 	}
 
-	public AuthStatus validateRequest(MessageInfo msgInfo, Subject client,
-			Subject server) throws AuthException {
-		try {
-			HttpServletRequest request = (HttpServletRequest) msgInfo
-					.getRequestMessage();
-			HttpServletResponse response = (HttpServletResponse) msgInfo
-					.getResponseMessage();
-			HttpSession session = request.getSession();
-			Assertion assertion = (Assertion) session
-					.getAttribute(CONST_CAS_ASSERTION);
-			if (assertion == null) {
-				service = CommonUtils.constructServiceUrl(request, response,
-						null, serverName, REQUEST_TICKET, false);
-				final String ticket = CommonUtils.safeGetParameter(request,
-						REQUEST_TICKET);
-				LoginContext lc = new LoginContext("cas",
-						new ServiceAndTicketCallbackHandler(service, ticket));
-				lc.login();
-				Subject subject = lc.getSubject();
-				for (Principal p : subject.getPrincipals()) {
-					if (p instanceof AssertionPrincipal) {
-						session.setAttribute(CONST_CAS_ASSERTION,
-								((AssertionPrincipal) p).getAssertion());
-						break;
-					}
-				}
-			}
-			return AuthStatus.SUCCESS;
-		} catch (Exception e) {
-			if (e.getCause() instanceof TicketValidationException) {
-				return AuthStatus.SEND_FAILURE;
-			}
-			return sendAuthenticateChallenge(msgInfo);
-		}
-	}
-
-	private AuthStatus sendAuthenticateChallenge(MessageInfo msgInfo) {
-		HttpServletResponse response = (HttpServletResponse) msgInfo
-				.getResponseMessage();
-		try {
-			response.sendRedirect(CommonUtils.constructRedirectUrl(casLoginUrl,
-					serviceParameterName, service, false, false));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if(sfc){
-			return AuthStatus.SUCCESS;
-		}else{
-			return AuthStatus.SEND_CONTINUE;
-		}
-		
-	}
-
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * javax.security.auth.message.ServerAuth#secureResponse(javax.security.
+	 * auth.message.MessageInfo, javax.security.auth.Subject)
+	 */
 	public AuthStatus secureResponse(MessageInfo msgInfo, Subject service)
 			throws AuthException {
-		return AuthStatus.SEND_SUCCESS;
+		return SEND_SUCCESS;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * javax.security.auth.message.ServerAuth#cleanSubject(javax.security.auth
+	 * .message.MessageInfo, javax.security.auth.Subject)
+	 */
 	public void cleanSubject(MessageInfo msgInfo, Subject subject)
 			throws AuthException {
 		if (subject != null) {
@@ -144,6 +118,147 @@ public class CasServerAuthModule implements ServerAuthModule {
 		}
 	}
 
-	private static final String AUTH_TYPE_INFO_KEY = "javax.servlet.http.authType";
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * javax.security.auth.message.module.ServerAuthModule#initialize(javax.
+	 * security.auth.message.MessagePolicy,
+	 * javax.security.auth.message.MessagePolicy,
+	 * javax.security.auth.callback.CallbackHandler, java.util.Map)
+	 */
+	public void initialize(MessagePolicy requestPolicy,
+			MessagePolicy responsePolicy, CallbackHandler handler, Map options)
+			throws AuthException {
+		this.requestPolicy = requestPolicy;
+		this.responsePolicy = responsePolicy;
+		this.handler = handler;
+		this.options = options;
+		if (options != null) {
+			this.service = (String) options.get(PROPERTY_SERVICE);
+			this.serverName = (String) options.get(PROPERTY_SERVER_NAME);
+			this.casServerUrlPrefix = (String) options
+					.get(PROPERTY_CAS_SERVER_URL_PREFIX);
+			this.casServerLoginUrl = (String) options
+					.get(PROPERTY_CAS_SERVER_LOGIN_URL);
+			if (options.containsKey(PROPERTY_JAAS_CONTEXT)) {
+				this.jaasContext = (String) options.get(PROPERTY_JAAS_CONTEXT);
+			}
+			if (options.containsKey(PROPERTY_DEFAULT_GROUPS)) {
+				String value = (String) options.get(PROPERTY_DEFAULT_GROUPS);
+				if (value != null) {
+					this.defaultGroups = value.split(",\\s*");
+				}
+			}
+			if (options.containsKey(PROPERTY_GROUP_ATTRIBUTE_NAMES)) {
+				String value = (String) options
+						.get(PROPERTY_GROUP_ATTRIBUTE_NAMES);
+				if (value != null) {
+					this.groupAttributeNames = value.split(",\\s*");
+				}
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * javax.security.auth.message.ServerAuth#validateRequest(javax.security
+	 * .auth.message.MessageInfo, javax.security.auth.Subject,
+	 * javax.security.auth.Subject)
+	 */
+	public AuthStatus validateRequest(MessageInfo msgInfo,
+			Subject clientSubject, Subject serverSubject) throws AuthException {
+		HttpServletRequest request = (HttpServletRequest) msgInfo
+				.getRequestMessage();
+		HttpServletResponse response = (HttpServletResponse) msgInfo
+				.getResponseMessage();
+		HttpSession session = request.getSession();
+		try {
+			Assertion assertion = (Assertion) session
+					.getAttribute(CONST_CAS_ASSERTION);
+			if (assertion != null) {
+				setAuthenticationResult(assertion, clientSubject, msgInfo);
+				return SUCCESS;
+			}
+			String ticket = CommonUtils.safeGetParameter(request,
+					this.artifactParameterName);
+			if (ticket == null || ticket.length() == 0) {
+				return sendRedirect(request, response);
+			}
+			LoginContext lc = new LoginContext(this.jaasContext,
+					new ServiceAndTicketCallbackHandler(service, ticket));
+			lc.login();
+			Subject subject = lc.getSubject();
+			for (Principal p : subject.getPrincipals()) {
+				if (p instanceof AssertionPrincipal) {
+					assertion = ((AssertionPrincipal) p).getAssertion();
+					session.setAttribute(CONST_CAS_ASSERTION, assertion);
+					if (this.redirectAfterValidation) {
+						logger.fine("Redirecting after successful ticket validation.");
+						return sendRedirect(request, response);
+					}
+					setAuthenticationResult(assertion, clientSubject, msgInfo);
+					return SUCCESS;
+				}
+			}
+			return SEND_FAILURE;
+		} catch (Exception e) {
+			if (e.getCause() instanceof TicketValidationException) {
+				logger.warning(e.getMessage());
+				try {
+					return sendRedirect(request, response);
+				} catch (IOException ioe) {
+					logger.throwing(CasServerAuthModule.class.getName(),
+							"validateRequest", ioe);
+				}
+			}
+			logger.throwing(CasServerAuthModule.class.getName(),
+					"validateRequest", e);
+			AuthException ae = new AuthException();
+			ae.initCause(e);
+			throw ae;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void setAuthenticationResult(Assertion assertion, Subject subject,
+			MessageInfo m) throws IOException, UnsupportedCallbackException {
+		if (assertion != null) {
+			Principal principal = assertion.getPrincipal();
+			this.handler.handle(new Callback[]{ new CallerPrincipalCallback(
+					subject, principal) });
+			m.getMap().put(AUTH_TYPE_INFO_KEY,
+					CasServerAuthModule.class.getName());
+			List groups = new ArrayList();
+			if (this.defaultGroups != null) {
+				groups.addAll(Arrays.asList(this.defaultGroups));
+			}
+			if (this.groupAttributeNames != null) {
+				for (String key : groupAttributeNames) {
+					String value = (String) assertion.getAttributes().get(key);
+					if (value != null) {
+						groups.addAll(Arrays.asList(value.split(",\\s*")));
+					}
+				}
+			}
+			if (groups.size() > 0) {
+				this.handler.handle(new Callback[]{ new GroupPrincipalCallback(
+						subject, (String[]) groups.toArray()) });
+			}
+		}
+	}
+
+	private String constructServiceUrl(HttpServletRequest request,
+			HttpServletResponse response) {
+		return CommonUtils.constructServiceUrl(request, response, this.service,
+				this.serverName, this.artifactParameterName,
+				this.encodeServiceUrl);
+	}
+
+	private AuthStatus sendRedirect(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		response.sendRedirect(constructServiceUrl(request, response));
+		return SEND_CONTINUE;
+	}
 
 }
